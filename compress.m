b0a8d86746885d6035072423f954c7cac0ca41fb
Luísa Menezes | Lume <83649500+clair-de-lume@users.noreply.github.com>
2022-05-31 11:30:45 -0300

    Update compress.m

--- compress.m
+++ compress.m
@@ -2,6 +2,9 @@ function compress(originalImg, k)
   img = imread (originalImg); % lê a imagem original e transforma numa matriz
   [linhas, colunas] = size(img)
   % ajuste da imagem aqui
+  n = (p+k)/(1+k) % calcular tamanho n da nova matriz
+  
+  
   lin_ini = 100; lin_fim = 150; col_ini = 100; col_fim = 150;
   img = imread("lena.png", "PixelRegion", {[lin_ini lin_fim], [col_ini col_fim]});
   imwrite (img, "compressed.png", "Compression", "none");
